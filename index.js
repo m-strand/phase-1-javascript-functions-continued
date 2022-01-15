26aab2f1bc6c22dd2303b15ee07041eb18659ca3
Brooke Strand <brooke.strand007@gmail.com>
2022-01-14 19:03:55 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function saturdayFun (someActivity = "roller-skate") { 
+    return "This Saturday, I want to " + someActivity + "!";
+}
+
+function mondayWork (someAction = "go to the office") {
+    return "This Monday, I will " + someAction + ".";
+}
+
+function wrapAdjective (specialFlair = "*") {
+    return function (howeverIwish="special") {
+        return `You are ${specialFlair}${howeverIwish}${specialFlair}!`;
+      };
+}
+ 
